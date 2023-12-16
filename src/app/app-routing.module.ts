1af13fd518b0e079fda2bd1203023cbdd360dcfa
Mohit Virli <mvirli@cohesity.com>
2023-12-16 15:05:30 +0530

    Base App component - refactor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChatComponent } from './chat/chat.component';
 
+/**
+ * The routes for the app
+ * /chat - new chat
+ * /chat/{id} - previous chat
+ */
 const routes: Routes = [
   { path: '', redirectTo: '/chat', pathMatch: 'full' },
   {
